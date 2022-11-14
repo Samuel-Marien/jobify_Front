f5fa1b0604d20acddeac7dca8f39095eca7d0aee
Samuel <timeuss.sam@gmail.com>
2022-11-14 17:10:06 +0100

    add navbar & links

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,32 @@
 import React from 'react'
+
+import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/SmallSidebar'
 
+import Logo from './Logo'
+import NavLinks from './NavLinks'
+
+import { FaTimes } from 'react-icons/fa'
+
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext()
   return (
     <Wrapper>
-      <h4>Small Navbar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   )
 }
