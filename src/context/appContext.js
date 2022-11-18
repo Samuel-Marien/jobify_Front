14d0a95ab98fd39a7cc81cafb1823f929a5ac41f
Samuel <timeuss.sam@gmail.com>
2022-11-18 14:37:51 +0100

    update user context&front

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -13,7 +13,10 @@ import {
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
   TOGGLE_SIDEBAR,
-  LOGOUT_USER
+  LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -143,10 +146,21 @@ const AppProvider = ({ children }) => {
   }
 
   const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN })
     try {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser)
-      console.log(data)
+      const { user, location, token } = data
+
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, location, token }
+      })
+      addUserToLocalStorage({ user, location, token })
     } catch (error) {
+      dispatch({
+        type: UPDATE_USER_ERROR,
+        payload: { msg: error.response.data.msg }
+      })
       // console.log(error.response)
     }
   }
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -8,7 +8,10 @@ import {
   LOGIN_USER_SUCCESS,
   LOGIN_USER_ERROR,
   TOGGLE_SIDEBAR,
-  LOGOUT_USER
+  LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR
 } from './actions'
 
 import { initialState } from './appContext'
@@ -101,6 +104,35 @@ const reducer = (state, action) => {
       token: null,
       userLocation: '',
       jobLocation: ''
+    }
+  }
+
+  // TODO
+  if (action.type === UPDATE_USER_BEGIN) {
+    return { ...state, isLoading: true }
+  }
+
+  if (action.type === UPDATE_USER_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      token: action.payload.token,
+      user: action.payload.user,
+      userLocation: action.payload.userLocation,
+      jobLocation: action.payload.jobLocation,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'User profile updated!'
+    }
+  }
+
+  if (action.type === UPDATE_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg
     }
   }
 
