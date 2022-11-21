d93b74b42f27bba1f136419905695276cb8df291
Samuel <timeuss.sam@gmail.com>
2022-11-21 23:47:54 +0100

    create job

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Alert from './Alert'
 import Navbar from './Navbar'
 import BigSidebar from './BigSidebar'
 import SmallSidebar from './SmallSidebar'
+import FormRowSelect from './FormRowSelect '
 
-export { FormRow, Logo, Alert, Navbar, BigSidebar, SmallSidebar }
+export { FormRow, Logo, Alert, Navbar, BigSidebar, SmallSidebar, FormRowSelect }
--- src/context/appContext.js
+++ src/context/appContext.js
@@ -16,7 +16,12 @@ import {
   LOGOUT_USER,
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
-  UPDATE_USER_ERROR
+  UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
+  CLEAR_VALUES,
+  CREATE_JOB_BEGIN,
+  CREATE_JOB_SUCCESS,
+  CREATE_JOB_ERROR
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -31,8 +36,16 @@ const initialState = {
   user: user ? JSON.parse(user) : null,
   token: token,
   userlocation: userlocation || '',
+  showSidebar: false,
+  isEditing: false,
+  editJob: '',
+  position: '',
+  company: '',
   jobLocation: userlocation || '',
-  showSidebar: false
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+  jobType: 'full-time',
+  statusOptions: ['interview', 'declined', 'pending'],
+  status: 'pending'
 }
 
 const AppContext = React.createContext()
@@ -166,6 +179,37 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const handleChange = ({ name, value }) => {
+    dispatch({ type: HANDLE_CHANGE, payload: { name, value } })
+  }
+
+  const clearValues = () => {
+    dispatch({ type: CLEAR_VALUES })
+  }
+
+  const createJob = async () => {
+    dispatch({ type: CREATE_JOB_BEGIN })
+    try {
+      const { position, company, jobLocation, jobType, status } = state
+      await authFetch.post('/jobs', {
+        position,
+        company,
+        jobLocation,
+        jobType,
+        status
+      })
+      dispatch({ type: CREATE_JOB_SUCCESS })
+      dispatch({ type: CLEAR_VALUES })
+    } catch (error) {
+      if (error.response.satus === 401) return
+      dispatch({
+        type: CREATE_JOB_ERROR,
+        payload: { msg: error.response.data.msg }
+      })
+    }
+    clearAlert()
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -176,7 +220,10 @@ const AppProvider = ({ children }) => {
         loginUser,
         toggleSidebar,
         logoutUser,
-        updateUser
+        updateUser,
+        handleChange,
+        clearValues,
+        createJob
       }}
     >
       {children}
